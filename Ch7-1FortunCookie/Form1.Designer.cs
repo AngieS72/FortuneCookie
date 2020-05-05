485bc162349ebf348ec8bdd4c3d5b4ff30955ad4
Angie Shelton <Angie_Shelton@insideranken.org>
2020-05-04 19:47:50 -0500

    first commit

---
+++ Ch7-1FortunCookie/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Ch7_1FortunCookie
+{
+    partial class FormFortuneCookie
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.labelFortunes = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(78, 12);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(169, 50);
+            this.buttonOpen.TabIndex = 0;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonClear.Location = new System.Drawing.Point(436, 12);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(169, 50);
+            this.buttonClear.TabIndex = 1;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(833, 12);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(169, 50);
+            this.buttonExit.TabIndex = 2;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // labelFortunes
+            // 
+            this.labelFortunes.BackColor = System.Drawing.Color.White;
+            this.labelFortunes.Location = new System.Drawing.Point(78, 83);
+            this.labelFortunes.Name = "labelFortunes";
+            this.labelFortunes.Size = new System.Drawing.Size(924, 156);
+            this.labelFortunes.TabIndex = 3;
+            // 
+            // FormFortuneCookie
+            // 
+            this.AcceptButton = this.buttonOpen;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.CancelButton = this.buttonClear;
+            this.ClientSize = new System.Drawing.Size(1147, 269);
+            this.Controls.Add(this.labelFortunes);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonOpen);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "FormFortuneCookie";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fortune Cookie";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Label labelFortunes;
+    }
+}
+
---
+++ Ch7-1FortunCookie/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ch7_1FortunCookie
+{
+    public partial class FormFortuneCookie : Form
+    {
+        public FormFortuneCookie()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            string myFortunes = generateFortune();
+
+            labelFortunes.Text = myFortunes;
+        }
+        private string generateFortune()
+        { 
+                string[] fortunes =
+        {
+            "Birthday are like friends.  The more you have the better.",
+            "Your smile is a treasure to all who know you.",
+            "You never hesitate to tackle the most difficult problems.",
+            "The most obvious solution is not always the best.",
+            "If you want the rainbows, you will have to tolerate the rain.",
+            "The early bird gets the work, the second mouse gets the cheese.",
+            " From listening comes wisdom and from speaking repentance.",
+            " A smile is your personal welcome mat.",
+        };
+
+
+        Random rand = new Random();     //will let me generate random numbers
+        int num1 = rand.Next(0, 7);
+            int num2 = rand.Next(0, 7);   //Generate random number between 0 -7
+
+        return (fortunes[num1] + "\n\n" + fortunes[num2]);
+            }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            labelFortunes.Text = "";
+            labelFortunes.Focus();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            quitProgramNow();
+        }
+        private void quitProgramNow()
+        {
+            DialogResult dialog = MessageBox.Show(
+                "Do you want to exit?",
+                "EXIT NOW?",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Warning);
+            if (dialog == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
